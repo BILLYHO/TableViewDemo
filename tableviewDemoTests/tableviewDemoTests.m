5f37cda34668af0956bf637de341c5ec84544181
BILLYHO <billyho0906@gmail.com>
2014-07-21 17:20:22 +0800

    init

---
+++ tableviewDemoTests/tableviewDemoTests.m
@@ -0,0 +1,34 @@
+//
+//  tableviewDemoTests.m
+//  tableviewDemoTests
+//
+//  Created by billy.ho on 7/15/14.
+//  Copyright (c) 2014 BILLYHO. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface tableviewDemoTests : XCTestCase
+
+@end
+
+@implementation tableviewDemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
